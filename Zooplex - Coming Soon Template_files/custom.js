dc8c55a5cc7c8a838819a84e26021d04da037712
akarac <ckara@visteon.com>
2015-07-15 09:59:53 +0200

    Initial commit

---
+++ Zooplex - Coming Soon Template_files/custom.js
@@ -0,0 +1,91 @@
+﻿(function($){
+   	
+   	// Preloader 	 
+   	$(window).load(function() { 
+       	$('#status').fadeOut();
+        $('#preloader').delay(350).fadeOut('slow'); 
+        $('body').delay(350).css({'overflow':'visible'});
+    }); 
+
+	$(document).ready(function() {
+		
+		// Image background
+		$.vegas({
+            src:'assets/images/IMG_6607_edit2.JPG'
+        });
+
+        $.vegas('overlay', {
+            src:'assets/images/06.png'
+        });
+
+		var countdown =  $('.countdown-time');
+
+		createTimeCicles();
+
+		$(window).on('resize', windowSize);
+
+		function windowSize(){
+			countdown.TimeCircles().destroy();
+		    createTimeCicles();
+			countdown.on('webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd', function() {
+        		countdown.removeClass('animated bounceIn');
+        	});
+		}
+
+		// TimeCicles - Create and Options
+		function createTimeCicles() {
+		    var langDay = {tr:'gün', en:'days'}
+			var langHour= {tr:'saat', en:'hours'}
+			var langMin = {tr:'dakika', en:'minutes'}
+			var langSec = {tr:'saniye', en:'seconds'}
+		     
+			countdown.addClass('animated bounceIn');
+			countdown.TimeCircles({
+				bg_width: 1,
+    			fg_width: 0.04,
+				circle_bg_color: '#bbb',
+				time: {
+    				    Days: {color: '#fff', text: langDay[lang]}
+    			,	   Hours: {color: '#fff', text: langHour[lang]}
+    			,	 Minutes: {color: '#fff', text: langMin[lang]}
+    			,	 Seconds: {color: '#fff', text: langSec[lang]}
+    			}
+			});
+			countdown.on('webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd', function() {
+        		countdown.removeClass('animated bounceIn');
+        	});
+		}
+
+		// Open modal window on click
+		$('.more-links a').on('click', function(e) {
+			var mainInner = $('.overlay'),
+				modal = $('#' + $(this).attr('data-modal'));
+					
+			mainInner.animate({opacity: 0}, 400, function(){
+				$('html,body').scrollTop(0);
+				modal.addClass('active').fadeIn(400);
+				countdown.TimeCircles().destroy();
+			});
+			e.preventDefault();
+
+			$('.modal-close').on('click', function(e) {
+				modal.removeClass('active').fadeOut(400, function(){
+					mainInner.animate({opacity: 1}, 400);
+		       		createTimeCicles();
+					countdown.on('webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd', function() {
+        				countdown.removeClass('animated bounceIn');
+        			});
+				});
+				e.preventDefault();
+			});
+		});
+
+		// Tooltips
+		$('.more-links a, .social a').tooltip();
+	
+		$('.more-links a, .social a').on('click', function () {
+			$(this).tooltip('hide')
+		});
+			
+	});
+})(jQuery);
